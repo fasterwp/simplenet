64e5f3f2e03029810723fde4886b237401506eaf
Andrei Chira <andreichira777@gmail.com>
2019-02-12 13:18:30 +0200

    rename theme

--- functions.php
+++ functions.php
@@ -14,9 +14,9 @@
 require_once get_template_directory() . '/lib/init.php';
 
 // Defines the child theme (do not remove).
-define( 'CHILD_THEME_NAME', 'Genesis Sample' );
+define( 'CHILD_THEME_NAME', 'Simplenet' );
 define( 'CHILD_THEME_URL', 'https://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.8.0' );
+define( 'CHILD_THEME_VERSION', '2.8.1' );
 
 // Sets up the Theme.
 require_once get_stylesheet_directory() . '/lib/theme-defaults.php';
