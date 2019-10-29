2a25d832bdf99180fe844b89363bce98cbd9dc9f
Andrei Chira <andreichira777@gmail.com>
2019-10-29 12:10:05 +0200

    back to graphic font

--- functions.php
+++ functions.php
@@ -70,13 +70,6 @@ add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
 function genesis_sample_enqueue_scripts_styles() {
 
 	wp_enqueue_style(
-		'google-fonts',
-		'//fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700&display=swap&subset=latin-ext',
-		array(),
-		CHILD_THEME_VERSION
-	);
-
-	wp_enqueue_style(
 	    'litedashicons',
 			get_stylesheet_directory_uri() . "/css/litedashicons.css",
 	    array(),
