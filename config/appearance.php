5d54c3192b9d40fc9ec963b5d0a25e6b9b806e31
Andrei Chira <andreichira777@gmail.com>
2021-09-14 23:52:46 +0300

    style front end gutenberg

--- config/appearance.php
+++ config/appearance.php
@@ -27,7 +27,7 @@ $genesis_sample_link_color_contrast   = genesis_sample_color_contrast( $genesis_
 $genesis_sample_link_color_brightness = genesis_sample_color_brightness( $genesis_sample_link_color, 35 );
 
 return [
-	'fonts-url'            => 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;800&family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+	'fonts-url'            => 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap',
 	'content-width'        => 1062,
 	'button-bg'            => $genesis_sample_link_color,
 	'button-color'         => $genesis_sample_link_color_contrast,
