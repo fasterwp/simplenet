9fbd45c74ce25b06de48ecf96ca89cef0730bf3c
Andrei Chira <andreichira777@gmail.com>
2021-10-14 17:28:59 +0300

    system fonts

--- config/appearance.php
+++ config/appearance.php
@@ -27,7 +27,7 @@ $genesis_sample_link_color_contrast   = genesis_sample_color_contrast( $genesis_
 $genesis_sample_link_color_brightness = genesis_sample_color_brightness( $genesis_sample_link_color, 35 );
 
 return [
-	'fonts-url'            => 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap',
+	'fonts-url'            => '',
 	'content-width'        => 1062,
 	'button-bg'            => $genesis_sample_link_color,
 	'button-color'         => $genesis_sample_link_color_contrast,
