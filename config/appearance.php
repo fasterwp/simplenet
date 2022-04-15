00633d75c68df18d1f5563fcda2a60e9cdde50da
Andrei Chira <andreichira777@gmail.com>
2022-04-16 01:06:18 +0300

    local fonts

--- config/appearance.php
+++ config/appearance.php
@@ -27,7 +27,7 @@ $genesis_sample_link_color_contrast   = genesis_sample_color_contrast( $genesis_
 $genesis_sample_link_color_brightness = genesis_sample_color_brightness( $genesis_sample_link_color, 35 );
 
 return [
-	'fonts-url'            => 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,700;1,400&display=swap',
+	'fonts-url'            => '',
 	'content-width'        => 1062,
 	'button-bg'            => $genesis_sample_link_color,
 	'button-color'         => $genesis_sample_link_color_contrast,
